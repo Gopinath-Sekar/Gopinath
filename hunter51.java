8ce60005b9d3a2890063ff020c6255b3d48b4dea
Gopinath-Sekar <gopinath.seee@ksrce.ac.in>
2016-08-22 20:35:20 +0530

    Create hunter51.java

---
+++ hunter51.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class hunter51
+{
+Scanner sc=new Scanner(System.in);
+String s1=sc.nextLine();
+String s2=sc.nextLine();
+int num1=Integer.paarseInt(s1);
+int num2=Integer.parseInt(s2);
+int mul=num1*num2;
+Srting output=String.valueOf(mul);
+System.out.println(output);
+}
